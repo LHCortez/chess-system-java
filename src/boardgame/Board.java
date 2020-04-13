cf995b4839e8fa017f31254f3ef24579f83d29e5
LHCortez <luizcquintas@gmail.com>
2020-04-13 17:32:15 -0300

    Movendo as peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,11 +43,28 @@ public class Board {
 
 	public void placePiece(Piece piece, Position position) {
 		if (ThereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position " + position+ ".");
+			throw new BoardException("There is already a piece on position " + position + ".");
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on board.");
+		}
+		if (piece(position) == null) {
+			return null;
+
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+
+		pieces[position.getRow()][position.getColumn()] = null;
+
+		return aux;
 	}
 
 	private boolean positionExists(int row, int column) {
